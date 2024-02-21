42137ca25f42fd063593b41e37d12522bf656698
J <59581793+daechi73@users.noreply.github.com>
2024-02-20 23:01:53 -0500

    implemented browserRouter, RouterProvider

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,27 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
+import SignInPage from "./components/pages/SignInPage.jsx";
+import SignUpPage from "./components/pages/SignUpPage.jsx";
+
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./index.css";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "/:url",
+    element: <App />,
+  },
+  // { path: "/sign_up", element: <SignUpPage /> },
+  // { path: "/home", element: <App /> },
+]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
