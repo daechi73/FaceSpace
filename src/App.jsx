bce5053c05b4af511823c93b5b650dee4ff3fde0
J <59581793+daechi73@users.noreply.github.com>
2024-02-20 23:00:11 -0500

    useParams implemented to render sign_in, sign_up, home, and pageNotFound

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,30 @@
 import { useState } from "react";
-import SignInPage from "./components/pages/SignInPage";
+import { useParams } from "react-router-dom";
 
+import SignInPage from "./components/pages/SignInPage";
+import SignUpPage from "./components/pages/SignUpPage";
+import PageNotFound from "./components/pages/PageNotFound";
+import LandingPage from "./components/pages/LandingPage";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const { url } = useParams();
 
-  return <SignInPage />;
+  const [count, setCount] = useState(0);
+  const [signedInUser, setSignedInUser] = useState("");
+  return (
+    <>
+      {url === undefined ? (
+        <SignInPage setSignedInUser={setSignedInUser} />
+      ) : url === "sign_up" ? (
+        <SignUpPage />
+      ) : url === "home" ? (
+        <LandingPage />
+      ) : (
+        <PageNotFound />
+      )}
+    </>
+  );
 }
 
 export default App;
