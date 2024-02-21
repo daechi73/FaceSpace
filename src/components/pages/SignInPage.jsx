b3bf12cc08d0e6394ae56e17a6d3f6ac60504b0c
J <59581793+daechi73@users.noreply.github.com>
2024-02-20 23:02:47 -0500

    Sign in system with username and password to server implemented

--- src/components/pages/SignInPage.jsx
+++ src/components/pages/SignInPage.jsx
@@ -1,20 +1,62 @@
 import React from "react";
+import { useState } from "react";
 import "./SignInPage.css";
-function SignInPage() {
+import { useNavigate } from "react-router-dom";
+function SignInPage(props) {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const navigate = useNavigate();
+
+  const handleSigninBtn = (e) => {
+    const options = {
+      mode: "cors",
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        username: username,
+        password: password,
+      }),
+    };
+    fetch("http://localhost:3000/users/sign_in", options)
+      .then((res) => res.json())
+      .then((res) => {
+        console.log(res);
+        if (res.status === "success") {
+          setUsername("");
+          setPassword("");
+          props.setSignedInUser(res.user);
+          navigate("/home");
+        }
+      });
+  };
+  const handleUsernameChange = (e) => {
+    setUsername(e.target.value);
+  };
+  const handlePasswordChange = (e) => {
+    setPassword(e.target.value);
+  };
   return (
     <div className="signInPage">
       <div className="signInForm">
         <input
           type="text"
           className="signInForm-userName"
-          placeholder="userName"
+          placeholder="Username"
+          name="username"
+          value={username}
+          onChange={handleUsernameChange}
         />
         <input
           type="password"
           className="signInForm-password"
           placeholder="password"
+          name="password"
+          value={password}
+          onChange={handlePasswordChange}
         />
-        <button className="signInBtn">Sign in</button>
+        <button className="signInBtn" onClick={handleSigninBtn}>
+          Sign in
+        </button>
       </div>
     </div>
   );
