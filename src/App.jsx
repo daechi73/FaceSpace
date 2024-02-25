8276f90efd2088ce1fe966996bf6c2f7a8a79ac1
J <59581793+daechi73@users.noreply.github.com>
2024-02-25 18:00:43 -0500

    added headers

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
-import { useState } from "react";
-import { useParams } from "react-router-dom";
-
 import SignInPage from "./components/pages/SignInPage";
 import SignUpPage from "./components/pages/SignUpPage";
 import PageNotFound from "./components/pages/PageNotFound";
 import LandingPage from "./components/pages/LandingPage";
+import Headers from "./components/global/headers/Headers";
+import { useState } from "react";
+import { useParams } from "react-router-dom";
+
 import "./App.css";
 
 function App() {
@@ -14,12 +15,13 @@ function App() {
   const [signedInUser, setSignedInUser] = useState("");
   return (
     <>
+      {url === undefined ? "" : <Headers signedInUser={signedInUser} />}
       {url === undefined ? (
         <SignInPage setSignedInUser={setSignedInUser} />
       ) : url === "sign_up" ? (
         <SignUpPage />
       ) : url === "home" ? (
-        <LandingPage />
+        <LandingPage signedInUser={signedInUser} />
       ) : (
         <PageNotFound />
       )}
