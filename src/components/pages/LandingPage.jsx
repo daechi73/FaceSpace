41a85ff1589eccbda0a4728fb439dbd41718ce19
J <59581793+daechi73@users.noreply.github.com>
2024-02-25 18:09:37 -0500

    added posts components for dev

--- src/components/pages/LandingPage.jsx
+++ src/components/pages/LandingPage.jsx
@@ -1,7 +1,13 @@
 import React from "react";
+import "./LandingPage.css";
+import Posts from "../sections/LandingPage/Posts";
 
 function LandingPage() {
-  return <div>LandingPage</div>;
+  return (
+    <div className="landingPage">
+      <Posts />
+    </div>
+  );
 }
 
 export default LandingPage;
