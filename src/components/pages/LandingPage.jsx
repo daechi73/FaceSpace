8d8c98ef1ccad4040cdcf039761ba1ba1cccd50d
J <59581793+daechi73@users.noreply.github.com>
2024-02-25 20:16:32 -0500

    added props to Posts

--- src/components/pages/LandingPage.jsx
+++ src/components/pages/LandingPage.jsx
@@ -2,10 +2,10 @@ import React from "react";
 import "./LandingPage.css";
 import Posts from "../sections/LandingPage/Posts";
 
-function LandingPage() {
+function LandingPage(props) {
   return (
     <div className="landingPage">
-      <Posts />
+      <Posts signedInUser={props.signedInUser} />
     </div>
   );
 }
