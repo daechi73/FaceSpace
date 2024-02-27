a01f25ce741c656e0af1c20ac9e7bdc9fd9ac8bc
J <59581793+daechi73@users.noreply.github.com>
2024-02-26 23:36:34 -0500

    dev mode: implemented Posts component

--- src/components/pages/LandingPage.jsx
+++ src/components/pages/LandingPage.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "./LandingPage.css";
-import Posts from "../sections/LandingPage/Posts";
+import Posts from "../sections/LandingPage/Posts/Posts";
 
 function LandingPage(props) {
   return (
