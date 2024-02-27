02775bed63778eb30394a1f7120e073ecc26cccd
J <59581793+daechi73@users.noreply.github.com>
2024-02-26 23:32:40 -0500

    implemented login persist through localStorage

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import SignUpPage from "./components/pages/SignUpPage";
 import PageNotFound from "./components/pages/PageNotFound";
 import LandingPage from "./components/pages/LandingPage";
 import Headers from "./components/global/headers/Headers";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 
 import "./App.css";
@@ -12,19 +12,52 @@ function App() {
   const { url } = useParams();
 
   const [count, setCount] = useState(0);
-  const [signedInUser, setSignedInUser] = useState("");
+  const [signedInUser, setSignedInUser] = useState();
+
+  useEffect(() => {
+    if (!signedInUser && localStorage.getItem("signedInUser")) {
+      console.log("here in app useEffect");
+      fetch(`http://localhost:3000/users/65d5664decf0c63384f7e002`, {
+        mode: "cors",
+      })
+        .then((res) => res.json())
+        .then((res) => {
+          console.log(res);
+          setSignedInUser(res.user);
+        });
+    }
+  });
+  console.log("here in app");
   return (
     <>
-      {url === undefined ? "" : <Headers signedInUser={signedInUser} />}
-      {url === undefined ? (
-        <SignInPage setSignedInUser={setSignedInUser} />
-      ) : url === "sign_up" ? (
-        <SignUpPage />
-      ) : url === "home" ? (
-        <LandingPage signedInUser={signedInUser} />
+      {signedInUser ? (
+        <>
+          <Headers signedInUser={signedInUser} />
+          {url === "home" ? (
+            <LandingPage signedInUser={signedInUser} />
+          ) : (
+            <PageNotFound />
+          )}
+        </>
       ) : (
-        <PageNotFound />
+        <>
+          {url === undefined ? (
+            <SignInPage setSignedInUser={setSignedInUser} />
+          ) : url === "sign_up" ? (
+            <SignUpPage />
+          ) : (
+            <PageNotFound />
+          )}
+        </>
       )}
+
+      <button
+        onClick={() => {
+          console.log(signedInUser);
+        }}
+      >
+        click
+      </button>
     </>
   );
 }
