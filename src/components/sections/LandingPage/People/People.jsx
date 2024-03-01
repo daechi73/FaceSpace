de8c6d86a2515f7daaec48486fb8dcfdb5efbb3f
J <59581793+daechi73@users.noreply.github.com>
2024-03-01 18:02:59 -0500

    basic people component

---
+++ src/components/sections/LandingPage/People/People.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./People.css";
+
+function People() {
+  return (
+    <div className="landingPage-people">
+      <div className="landingPage-people-user-wrapper">
+        <div className="landingPage-people-user">userHere</div>
+        <div className="landingPage-people-user-addBtn">+</div>
+      </div>
+    </div>
+  );
+}
+
+export default People;
