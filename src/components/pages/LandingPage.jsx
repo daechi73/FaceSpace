8c1cc48b21187354baeae10011682c158461bd21
J <59581793+daechi73@users.noreply.github.com>
2024-03-03 17:58:17 -0500

    props passthrough

--- src/components/pages/LandingPage.jsx
+++ src/components/pages/LandingPage.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import "./LandingPage.css";
 import Posts from "../sections/LandingPage/Posts/Posts";
+import People from "../sections/LandingPage/People/People.jsx";
 
 function LandingPage(props) {
   return (
     <div className="landingPage">
       <Posts signedInUser={props.signedInUser} />
+      <People signedInUser={props.signedInUser} />
     </div>
   );
 }
