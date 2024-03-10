749dc4b355e71adf98bfe4ea741c2a9c7c2acbf4
J <59581793+daechi73@users.noreply.github.com>
2024-03-10 17:39:28 -0400

    users render in division based on friend request status

--- src/components/sections/LandingPage/People/People.jsx
+++ src/components/sections/LandingPage/People/People.jsx
@@ -4,6 +4,8 @@ import "./People.css";
 
 function People(props) {
   const [people, setPeople] = useState([]);
+  console.log("here in people");
+
   useEffect(() => {
     const options = {
       mode: "cors",
@@ -14,21 +16,91 @@ function People(props) {
     fetch("http://localhost:3000/users/other_users", options)
       .then((res) => res.json())
       .then((res) => {
-        console.log(res);
+        console.log("in people useEffect");
+        // console.log(res);
         if (res.status === "success") setPeople(res.users);
       });
   }, []);
   const renderUsers = people.map((e, i) => {
+    const addFriend = () => {
+      const options = {
+        mode: "cors",
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ toAddUserId: e._id }),
+      };
+      fetch(
+        `http://localhost:3000/users/${props.signedInUser._id}/update/addFriend`,
+        options
+      )
+        .then((res) => res.json())
+        .then((res) => {
+          console.log("In renderUSers add friend");
+          props.setSignedInUser(res.user);
+        });
+    };
+    const checkUserAdded = () => {
+      for (
+        let j = 0;
+        j < props.signedInUser.friend_requests_outgoing.length;
+        j++
+      ) {
+        if (props.signedInUser.friend_requests_outgoing[j]._id === e._id) {
+          return true;
+        }
+      }
+      return false;
+    };
+
     return (
       <div className="landingPage-people-user" key={i}>
         <div className="landingPage-People-user-user">{e.user_name}</div>
-        <div className="landingPage-people-user-addBtn">+</div>
+        {checkUserAdded() ? (
+          <div className="landingPage-people-user-waitingForResponse">
+            Waiting for response
+          </div>
+        ) : (
+          <div className="landingPage-people-user-addBtn" onClick={addFriend}>
+            +
+          </div>
+        )}
       </div>
     );
   });
+
+  const renderFriends =
+    props.signedInUser.friends.length === 0
+      ? "You have no friends"
+      : props.signedinUser.friends.map((e, i) => {
+          return (
+            <div className="landingPage-people-friends" key={i}>
+              {e.user_name}
+            </div>
+          );
+        });
+  const renderFriendReq =
+    props.signedInUser.friend_requests_incoming.length === 0
+      ? ""
+      : props.signedInUser.friend_requests_incoming.map((e, i) => {
+          return (
+            <div className="landingPage-people-friends-request" key={i}>
+              {e.user_name}
+              <div className="landingPage-people-friends-request-acceptBtn">
+                Accept
+              </div>
+              <div className="landingPage-people-friends-request-declineBtn">
+                decline
+              </div>
+            </div>
+          );
+        });
   return (
     <div className="landingPage-people">
-      <div className="landingPage-people-user-wrapper">{renderUsers}</div>
+      <div className="landingPage-people-friends-container">
+        {renderFriendReq}
+        {renderFriends}
+      </div>
+      <div className="landingPage-people-user-container">{renderUsers}</div>
     </div>
   );
 }
