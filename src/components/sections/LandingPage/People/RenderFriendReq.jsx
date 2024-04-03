72859016bfd26e84d6bd01ccfd808834f2db96ef
J <59581793+daechi73@users.noreply.github.com>
2024-04-02 23:12:03 -0400

    implemented handleAcceptFReq function that adds involved users as friends; added a dev console.log

--- src/components/sections/LandingPage/People/RenderFriendReq.jsx
+++ src/components/sections/LandingPage/People/RenderFriendReq.jsx
@@ -1,19 +1,37 @@
 import React from "react";
 
 function RenderFriendReq(props) {
-  const handleAcceptFReq = () => {
-    console.log("working");
-  };
-  const handleDeclineFreq = () => {
-    console.log("working");
-  };
-
+  console.log("Here in RenderFriendReq");
   const renderFriendReq =
     props.signedInUser.friend_requests.length === 0
       ? ""
       : props.signedInUser.friend_requests.map((e, i) => {
           if (e.inbound._id === props.signedInUser._id) {
             console.log("working in renderFRiendReq");
+
+            const handleAcceptFReq = () => {
+              console.log("working");
+              const options = {
+                mode: "cors",
+                headers: { "Content-Type": "application/json" },
+                method: "POST",
+                body: JSON.stringify({ friendReq: e }),
+              };
+              fetch(
+                `http://localhost:3000/users/${props.signedInUser._id}/update/addFriend`,
+                options
+              )
+                .then((res) => res.json())
+                .then((res) => {
+                  if (res.status === "success") {
+                    console.log(res);
+                    props.setSignedInUser(res.user);
+                  }
+                });
+            };
+            const handleDeclineFreq = () => {
+              console.log("working");
+            };
             return (
               <div className="landingPage-people-friends-request" key={i}>
                 {e.outbound.user_name}
