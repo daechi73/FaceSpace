dca115cd1a23d4202f770ec6b27e01f5cbdf00af
J <59581793+daechi73@users.noreply.github.com>
2024-04-06 20:41:02 -0400

    Linked the headers title to home page

--- src/components/global/headers/Headers.jsx
+++ src/components/global/headers/Headers.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 import "./Headers.css";
 import Profile from "./Profile";
+import { Link } from "react-router-dom";
 
 function Headers(props) {
   return (
     <div className="headers">
-      <div className="headers-title">FaceSpace</div>
+      <div className="headers-title">
+        <Link to="/home">FaceSpace</Link>
+      </div>
       <Profile
         signedInUser={props.signedInUser.user_name}
         setSignedInUser={props.setSignedInUser}
