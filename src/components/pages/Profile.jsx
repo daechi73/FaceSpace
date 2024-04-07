65d462c6b0355b9c246b33bd61091b2ea3d8550d
J <59581793+daechi73@users.noreply.github.com>
2024-04-07 16:14:01 -0400

    changed props name profileUser->userProfile; renders user's bio, posts and friends

--- src/components/pages/Profile.jsx
+++ src/components/pages/Profile.jsx
@@ -1,27 +1,33 @@
 import React from "react";
 import "./Profile.css";
 import Posts from "../sections/Profile/Posts";
+import RenderFriends from "../sections/LandingPage/People/RenderFriends";
 import { useEffect, useState } from "react";
 
 function Profile(props) {
   console.log("In Profile");
-  const [profileUser, setProfileUser] = useState();
+  const [userProfile, setUserProfile] = useState();
 
   useEffect(() => {
     fetch(`http://localhost:3000/users/username/${props.userProfile}`)
       .then((res) => res.json())
       .then((res) => {
         console.log(res);
-        setProfileUser(res.user);
+        setUserProfile(res.user);
       });
   }, []);
-  console.log(profileUser);
+  console.log(userProfile);
   return (
     <div className="profile-container">
-      {profileUser ? (
+      {userProfile ? (
         <>
-          <div className="profile-username">{profileUser.user_name}</div>
-          <Posts posts={profileUser.posts} />
+          <div className="profile-username">{userProfile.user_name}</div>
+          <div className="profile-bio">{userProfile.bio}</div>
+          <Posts posts={userProfile.posts} />
+          <RenderFriends
+            user={userProfile}
+            setUserProfile={props.setUserProfile}
+          />
         </>
       ) : (
         ""
