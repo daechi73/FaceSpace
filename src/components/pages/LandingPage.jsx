48e76de3f74591904c767cb2088c1b4d4556291c
J <59581793+daechi73@users.noreply.github.com>
2024-04-09 22:41:33 -0400

    State pass through

--- src/components/pages/LandingPage.jsx
+++ src/components/pages/LandingPage.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import "./LandingPage.css";
 import Posts from "../sections/LandingPage/Posts/Posts";
 import People from "../sections/LandingPage/People/People.jsx";
+import Chatbox from "../global/Chatbox/Chatbox.jsx";
 
 function LandingPage(props) {
   console.log("here in landingPage");
@@ -12,7 +13,10 @@ function LandingPage(props) {
         signedInUser={props.signedInUser}
         setSignedInUser={props.setSignedInUser}
         setUserProfile={props.setUserProfile}
+        people={props.people}
+        setPeople={props.setPeople}
       />
+      <Chatbox />
     </div>
   );
 }
