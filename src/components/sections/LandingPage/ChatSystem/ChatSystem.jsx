115b2cf2b80c154e23b2890179e9e973a512231d
J <59581793+daechi73@users.noreply.github.com>
2024-07-03 21:05:16 -0400

    adds alternative classname for that comes true to new_messages

--- src/components/sections/LandingPage/ChatSystem/ChatSystem.jsx
+++ src/components/sections/LandingPage/ChatSystem/ChatSystem.jsx
@@ -7,18 +7,26 @@ import "./ChatSystem.css";
 function ChatSystem(props) {
   console.log("HERE in ChatSystem");
   const users = useGetChatUsers(props.signedInUser._id);
+  console.log("heree1");
+  console.log(users);
 
   const renderUsers = users.map((e, i) => {
     const handleUserClick = () => {
-      props.setChatUsers([props.signedInUser._id, e.user_name]);
+      props.setChatUsers([props.signedInUser._id, e.user.user_name]);
       if (props.chatbox === null) {
         props.setChatbox(1);
       } else props.setChatbox(props.chatbox + 1);
     };
 
     return (
-      <div className="chatSystem_user" key={i} onClick={handleUserClick}>
-        {e.user_name}
+      <div
+        className={
+          e.new_message ? "chatSystem_user newMessages" : "chatSystem_user"
+        }
+        key={i}
+        onClick={handleUserClick}
+      >
+        {e.user.user_name}
       </div>
     );
   });
