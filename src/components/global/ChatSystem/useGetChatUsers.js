80791453b04114b5889ad4ebccae6b94a1ad92c0
J <59581793+daechi73@users.noreply.github.com>
2024-08-01 20:09:06 -0400

    changed ChatSystem location to global folder

---
+++ src/components/global/ChatSystem/useGetChatUsers.js
@@ -0,0 +1,34 @@
+import { useState, useEffect } from "react";
+
+const userGetChatUsers = (signedInUserId, resetChatSystem) => {
+  const [users, setUsers] = useState([]);
+  let chatboxes;
+
+  useEffect(() => {
+    fetch(`http://localhost:3000/users/${signedInUserId}`)
+      .then((res) => res.json())
+      .then((res) => {
+        chatboxes = res.user.chatbox;
+        const tempUsers = [];
+
+        const findOtherUser = (users) => {
+          for (let i = 0; i < users.length; i++) {
+            if (users[i].user_name !== res.user.user_name) {
+              return users[i];
+            }
+          }
+        };
+
+        chatboxes.forEach((e) => {
+          const otherUser = findOtherUser(e.users);
+          if (res.user.user_name === e.new_message || e.new_message === "") {
+            tempUsers.push({ user: otherUser, new_message: false });
+          } else tempUsers.push({ user: otherUser, new_message: true });
+          setUsers(tempUsers);
+        });
+      });
+  }, [resetChatSystem]);
+  return users;
+};
+
+export default userGetChatUsers;
