a044a1126098429bc87170e659db72c761c43e6b
J <59581793+daechi73@users.noreply.github.com>
2024-09-25 15:26:18 -0400

    classname redone

--- src/components/global/People/RenderUsersUtilify/RenderFriends.jsx
+++ src/components/global/People/RenderUsersUtilify/RenderFriends.jsx
@@ -1,15 +1,27 @@
 import React from "react";
 import ProfileLink from "../../../sections/Profile/ProfileLink";
+import "./RenderFriends.css";
 
 function RenderFriends(props) {
   console.log("here in RenderFriends");
   const renderFriends =
     props.user.friends.length === 0 ? (
-      <div className="landingPage-people-friends">"You have no friends"</div>
+      <div
+        className={
+          props.className + "-friends-noFriends" + " renderFriends-friends"
+        }
+      >
+        "You have no friends"
+      </div>
     ) : (
       props.user.friends.map((e, i) => {
         return (
-          <div className="landingPage-people-friends" key={i}>
+          <div
+            className={
+              props.className + "-friends-friend" + " renderFriends-friend"
+            }
+            key={i}
+          >
             <ProfileLink
               username={e.user_name}
               setUserProfile={props.setUserProfile}
@@ -20,7 +32,14 @@ function RenderFriends(props) {
         );
       })
     );
-  return <>{renderFriends}</>;
+  return (
+    <div className={props.className + "-friends" + " renderFriends-friends"}>
+      <div className={props.className + "-friends-title"}>Friends</div>
+      <div className={props.className + "-friends-friends"}>
+        {renderFriends}
+      </div>
+    </div>
+  );
 }
 
 export default RenderFriends;
