1aba78ded694c212c025bf2235cffe12a38e3e39
J <59581793+daechi73@users.noreply.github.com>
2024-10-01 17:45:15 -0400

    sets relative position to user click position at render

--- src/components/global/UserClickMenu/RenderUserClickMenu.jsx
+++ src/components/global/UserClickMenu/RenderUserClickMenu.jsx
@@ -1,8 +1,18 @@
 import React from "react";
 import "./RenderUserClickMenu.css";
 import ProfileLink from "../../sections/Profile/ProfileLink";
+import { useRef, useEffect } from "react";
 
 function RenderUserClickMenu(props) {
+  const userClickMenu = useRef();
+
+  useEffect(() => {
+    console.log("inRenderUserClickMenu useEffect");
+    const userClickMenuDiv = userClickMenu.current;
+    userClickMenuDiv.style.top = `${props.coords[1] + 10}px`;
+    userClickMenuDiv.style.left = `${props.coords[0] + 10}px`;
+  }, []);
+
   const handleChatClick = () => {
     if (props.chatbox === null) props.setChatbox(1);
     else props.setChatbox(props.chatbox + 1);
@@ -11,7 +21,7 @@ function RenderUserClickMenu(props) {
     props.setChatUsers(chatUsers);
   };
   return (
-    <div className="userClickMenu">
+    <div className="userClickMenu" ref={userClickMenu}>
       <div className="userClickMenu-chat" onClick={handleChatClick}>
         Chat
       </div>
