033776bb8eec254a3fac9fb1263f1d617332384b
J <59581793+daechi73@users.noreply.github.com.>
2024-10-17 19:09:38 -0400

    chatsystem scrollbar done

--- src/components/global/ChatSystem/ChatSystem.jsx
+++ src/components/global/ChatSystem/ChatSystem.jsx
@@ -65,25 +65,7 @@ function ChatSystem(props) {
   return (
     <div className="chatSystem">
       <div className="chatSystem-title">Chat</div>
-      <div className="chatSystem-users">
-        {renderUsers}
-        <div className="chatSystem-user-username">jjaa</div>
-        <div className="chatSystem-user-username">jjaa</div>
-        <div className="chatSystem-user-username">jjaa</div>
-        <div className="chatSystem-user-username">jjaa</div>
-        <div className="chatSystem-user-username">jjaa</div>v
-        <div className="chatSystem-user-username">jjaa</div>
-        <div className="chatSystem-user-username">jjaa</div>
-        <div className="chatSystem-user-username">jjaa</div>
-        <div className="chatSystem-user-username">jjaa</div>
-        <div className="chatSystem-user-username">jjaa</div>v
-        <div className="chatSystem-user-username">jjaa</div>v
-        <div className="chatSystem-user-username">jjaa</div>
-        <div className="chatSystem-user-username">jjaa</div>
-        <div className="chatSystem-user-username">jjaa</div>
-        <div className="chatSystem-user-username">jjaa</div>
-        <div className="chatSystem-user-username">jjaa</div>
-      </div>
+      <div className="chatSystem-users">{renderUsers}</div>
     </div>
   );
 }
